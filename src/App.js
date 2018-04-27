f9405abfa5fa7c271c821bb7a99ed1d48231580c
payalcsureja <payalcsureja@gmail.com>
2018-04-27 15:41:51 -0400

    env variable setup

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <div className={styles.App}>
           <header className={styles['App-header']}>
             <img src={logo} className={styles['App-logo']} alt="logo" />
-            <h1 className={styles['App-title']}>Welcome to React!</h1>
+            <h1 className={styles['App-title']}>Welcome to {process.env.REACT_APP_TITLE}!</h1>
           </header>
           <p className={styles['App-intro']}>
             To get started, edit <code>src/App.js</code> and save to reload.
